dbb9d6ace47236d1ce62f81eb4e3b9ee80f6b6cc
“julian <badjab326@gmail.com>
2023-11-06 13:11:49 -0800

    update: file size reduced

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// Import Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route } from "react-router-dom";
+// Import Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bgdarken">
+        <Header />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Footer />
+        <iframe id="myVideo" src="https://streamable.com/e/5xmxw6?autoplay=1&nocontrols=1" frameborder="0" width="1920px" height="1080px" allowfullscreen allow="autoplay"></iframe>
+        {/* <video autoPlay muted loop id="myVideo">
+          <source src={background} type="video/mp4" />
+        </video> */}
+      </div>
     </div>
   );
 }
