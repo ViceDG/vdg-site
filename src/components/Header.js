a66d722797f528ba45d56358b1e917010d023acd
“julian <badjab326@gmail.com>
2023-11-10 12:53:35 -0800

    update: navbar fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,8 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Header = (props) => {
-
   return (
-    <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+    <nav>
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" class="flex items-center">
           <img
@@ -16,7 +15,7 @@ const Header = (props) => {
         <button
           data-collapse-toggle="navbar-solid-bg"
           type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden focus:outline-none hover:ring-2 hover:ring-gray-900 focus:ring-2 focus:ring-gray-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-solid-bg"
           aria-expanded="false"
         >
@@ -37,15 +36,12 @@ const Header = (props) => {
             />
           </svg>
         </button>
-        <div
-          class="hidden w-full lg:block lg:w-auto"
-          id="navbar-solid-bg"
-        >
-          <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 lg:flex-row lg:space-x-8 lg:mt-0 lg:border-0 lg:bg-transparent dark:bg-gray-800 lg:dark:bg-transparent dark:border-gray-700">
+        <div class="hidden w-full lg:block lg:w-auto" id="navbar-solid-bg">
+          <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-900 lg:flex-row lg:space-x-8 lg:mt-0 lg:border-0 lg:bg-transparent dark:bg-gray-800 lg:dark:bg-transparent dark:border-gray-700">
             <li>
               <a
                 href="/"
-                class="navLinks block py-2 pl-3 pr-4 text-white bg-blue-700 rounded lg:bg-transparent lg:text-blue-700 lg:p-0 lg:dark:text-blue-500 dark:bg-blue-600 lg:dark:bg-transparent"
+                class="navLinks block border-solid border-2 border-gray-500 py-2 pl-3 pr-4 text-white-900 bg-gray-900 rounded lg:border-0 lg:bg-transparent lg:text-blue-700 lg:p-0 lg:dark:text-blue-500 dark:bg-blue-600 lg:dark:bg-transparent"
                 aria-current="page"
               >
                 Home
@@ -54,7 +50,7 @@ const Header = (props) => {
             <li>
               <a
                 href="/about"
-                class="navLinks block py-2 pl-3 pr-4 text-white-900 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent"
+                class="navLinks block border-solid border-2 border-gray-500 py-2 pl-3 pr-4 text-white-900 bg-gray-900 rounded lg:border-0 lg:bg-transparent lg:text-blue-700 lg:p-0 lg:dark:text-blue-500 dark:bg-blue-600 lg:dark:bg-transparent"
               >
                 About
               </a>
@@ -62,7 +58,7 @@ const Header = (props) => {
             <li>
               <a
                 href="/projects"
-                class="navLinks block py-2 pl-3 pr-4 text-white-900 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent"
+                class="navLinks block border-solid border-2 border-gray-500 py-2 pl-3 pr-4 text-white-900 bg-gray-900 rounded lg:border-0 lg:bg-transparent lg:text-blue-700 lg:p-0 lg:dark:text-blue-500 dark:bg-blue-600 lg:dark:bg-transparent"
               >
                 Projects
               </a>
@@ -70,7 +66,7 @@ const Header = (props) => {
             <li>
               <a
                 href="/contact"
-                class="navLinks block py-2 pl-3 pr-4 text-white-900 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent"
+                class="navLinks block border-solid border-2 border-gray-500 py-2 pl-3 pr-4 text-white-900 bg-gray-900 rounded lg:border-0 lg:bg-transparent lg:text-blue-700 lg:p-0 lg:dark:text-blue-500 dark:bg-blue-600 lg:dark:bg-transparent"
               >
                 Contact
               </a>
@@ -83,5 +79,3 @@ const Header = (props) => {
 };
 
 export default Header;
-
-<img className="logo" title="Home" alt="Home" src="/images/mainLogo.png" />;
