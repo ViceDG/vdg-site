95a2fac9e004855b8f0cb87b9591f4f791728be9
“julian <badjab326@gmail.com>
2023-11-10 15:06:37 -0800

    update: projects page fixes

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -28,40 +28,43 @@ const Projects = () => {
 
   const projectGrid = ProjectsData.map((project) => (
     <div className="item">
-      <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img onClick={() => openModal(project.live)} class="rounded-t-lg" src={project.image}
-        alt={project.name} />
-    </a>
-    <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{project.name}</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{project.description}</p>
-        <div className="projLinks">
-        <a target="_blank" rel="noreferrer" href={project.git}>
-        <img
-          className="icons"
-          src="/images/githubLogoMini.png"
-          alt="GitHub Link"
-        ></img>
-      </a>
-      <a target="_blank" rel="noreferrer" href={project.live}>
-        <img
-          className="icons"
-          src="/images/webLogo.png"
-          alt="Live Site Link"
-        ></img>
-      </a>
+      <div class="max-w-sm bg-white border-solid border-2 border-gray-400 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <img
+            onClick={() => openModal(project.live)}
+            class="rounded-t-lg border-solid border-2 border-gray-400"
+            src={project.image}
+            alt={project.name}
+          />
+        <div class="p-5">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {project.name}
+            </h5>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            {project.description}
+          </p>
+          <div className="bioLinks">
+            <a target="_blank" rel="noreferrer" href={project.git}>
+              <img
+                className="icons"
+                src="/images/githubLogoMini.png"
+                alt="GitHub Link"
+              ></img>
+            </a>
+            <a target="_blank" rel="noreferrer" href={project.live}>
+              <img
+                className="icons"
+                src="/images/webLogo.png"
+                alt="Live Site Link"
+              ></img>
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-</div>
-
     </div>
   ));
 
   return (
-    <div className="projects">
+    <div className="about">
       <h1 className="projTitle">PROJECTS</h1>
       <div className="projectsCardsContainer">
         {projectGrid}
